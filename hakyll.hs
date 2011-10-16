f32f43f1c871af21d03ea5a4682baa8bd6c75daa
Ben Gamari <bgamari.foss@gmail.com>
2011-10-16 13:43:10 -0400

    Use Hakyll

---
+++ hakyll.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Hakyll
+import Control.Arrow ((>>>), (***), arr)
+import Data.Monoid (mempty, mconcat)
+
+main :: IO ()
+main = hakyll $ do
+        match "css/*" $ do
+                route   idRoute
+                compile compressCssCompiler
+
+        match "posts/*.mkd" $ do
+                route   $ setExtension "html"
+                compile $ pageCompiler
+                        >>> applyTemplateCompiler "templates/default.html"
+                        >>> relativizeUrlsCompiler
+
+        match "index.html" $ route idRoute
+        create "index.html" $ constA mempty
+                >>> arr (setField "title" "Home")
+                >>> requireAllA "posts/*.mkd" addPostList
+                >>> applyTemplateCompiler "templates/index.html"
+                >>> applyTemplateCompiler "templates/default.html"
+                >>> relativizeUrlsCompiler
+
+        match "templates/*" $ compile templateCompiler
+
+
+addPostList :: Compiler (Page String, [Page String]) (Page String)
+addPostList = setFieldA "posts" $
+    arr (reverse . sortByBaseName)
+        >>> require "templates/postitem.html" (\p t -> map (applyTemplate t) p)
+        >>> arr mconcat
+        >>> arr pageBody
+
